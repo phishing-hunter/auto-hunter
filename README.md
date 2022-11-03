12d66327ec5f23e6091ae011bb86f7edd0989278
tatsui <tatsuya.ito.at@gmail.com>
2022-11-03 05:06:06 +0000

    キーワード設定

--- README.md
+++ README.md
@@ -6,13 +6,15 @@
 ## 動作に必要な情報
 以下のサービスに登録しAPIキーを取得してください。
 
-* https://phishing-hunter.com
+* [phishing-hunter](https://phishing-hunter.com)
 	* API Key
-* https://ngrok.com/
+* [ngrok](https://ngrok.com/)
 	* API Key
 	* Auth Key
-* https://urlscan.io/
+* [urlscan.io](https://urlscan.io/)
 	* API Key
+* [slack](https://slack.com/intl/ja-jp/help/articles/115005265063-Slack-%E3%81%A7%E3%81%AE-Incoming-Webhook-%E3%81%AE%E5%88%A9%E7%94%A8)
+	* Incoming Webhook
 
 ## 環境変数の設定
 ```bash
@@ -21,6 +23,13 @@ $ echo "NGROK_API=ngrokのAPI key NGROK_AUTHではありません" >> .env
 $ echo "API_BASE_URL=https://h99x7kgf7k.execute-api.ap-northeast-1.amazonaws.com/prod" >> .env
 $ echo "PH_API_KEY=phishing hunter のAPIキー" >> .env
 $ echo "URLSCAN_API=urlscan.ioのAPIキー" >> .env
+$ echo "SLACK_WEBHOOK_URL=urlscan.ioのAPIキー" >> .env
+```
+
+## 監視キーワードの設定
+```
+$ cp config.yml.template config.yml
+$ vim config.yml
 ```
 
 ## dockerの起動
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import json
 import time
+import yaml
 import ngrok
 import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -9,21 +10,25 @@ PH_API_KEY = os.environ.get("PH_API_KEY")
 API_BASE_URL = os.environ.get("API_BASE_URL")
 NGROK_API = os.environ.get("NGROK_API")
 URLSCAN_API = os.environ.get("URLSCAN_API", "")
+SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL", "")
 
 headers = {"authorization": PH_API_KEY}
 req = requests.get(f"{API_BASE_URL}/users/info", headers=headers)
 user_info = req.json()
-LIMIT = user_info['quota']['observations']
+LIMIT = user_info["quota"]["observations"]
+
 
 def get_observation():
     req = requests.get(f"{API_BASE_URL}/observation", headers=headers)
     return req.json()
 
+
 def add_observation(domain):
-    url = f'https://{domain}'
-    body = {"url":url,"interval":"hour","period":3,"comment":"auto submit"}
+    url = f"https://{domain}"
+    body = {"url": url, "interval": "hour", "period": 3, "comment": "registered from auto-hunter"}
     req = requests.post(f"{API_BASE_URL}/observation/add", data=json.dumps(body), headers=headers)
     assert req.json()["result"] == True
+    print(f"add observation url: {url}")
 
 def set_webhook_url():
     client = ngrok.Client(NGROK_API)
@@ -37,49 +42,70 @@ def set_webhook_url():
     assert public_url != ""
 
     webhook_url = f"{public_url}/webhook"
-    print(f"set {webhook_url}")
+    print(f"set keyword webhook: {webhook_url}")
     headers = {"authorization": PH_API_KEY}
-    body = {"type":"custom","webhook_url":webhook_url,"service":"keyword"} 
+    body = {"type": "custom", "webhook_url": webhook_url, "service": "keyword"}
+    req = requests.post(f"{API_BASE_URL}/notify", data=json.dumps(body), headers=headers)
+    assert req.json()["result"] == True
+    print(f"set observation webhook: {SLACK_WEBHOOK_URL}")
+    body = {"type": "slack", "webhook_url": SLACK_WEBHOOK_URL, "service": "observation"}
     req = requests.post(f"{API_BASE_URL}/notify", data=json.dumps(body), headers=headers)
     assert req.json()["result"] == True
 
+
+def set_keywords(conf):
+    print(f"set keywords {conf['keywords']}")
+    headers = {"authorization": PH_API_KEY}
+    body = {"keywords": conf["keywords"], "target_score": conf["target_score"]}
+    req = requests.post(f"{API_BASE_URL}/keywords", data=json.dumps(body), headers=headers)
+    assert req.json()["result"] == True
+
+
 def set_urlscan_api():
     headers = {"authorization": PH_API_KEY}
-    body = {"api_keys":{"urlscan":URLSCAN_API}}
+    body = {"api_keys": {"urlscan": URLSCAN_API}}
     req = requests.post(f"{API_BASE_URL}/apikeys", data=json.dumps(body), headers=headers)
     print(f"set urlscan.io apikey ({URLSCAN_API})")
     assert req.json()["result"] == True
 
+
 class BaseHttpServer(BaseHTTPRequestHandler):
     def do_POST(self):
         body = ""
-        print(dir(self))
-        try:
-            content_len=int(self.headers.get('content-length').encode())
-            requestBody = self.rfile.read(content_len).decode('utf-8')
-            body = json.loads(requestBody)
-            with open("/logs/log.json", "a") as f:
-                f.write(f"{requestBody}\n")
-            for report in body["reports"]:
-                obs = get_observation()
-                used = len(obs["observation_urls"].keys())
-                if LIMIT > used:
-                    add_observation(report["domain"])
-                time.sleep(0.1)
-            body = "OK"
-            self.send_response(200)
-        except Exception as e:
-            body = "request error"
-            self.send_response(400)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('content-length', len(body))
+        statusCode = 404
+        if self.path == "/webhook":
+            try:
+                content_len = int(self.headers.get("content-length").encode())
+                requestBody = self.rfile.read(content_len).decode("utf-8")
+                body = json.loads(requestBody)
+                with open("/logs/log.json", "a") as f:
+                    f.write(f"{requestBody}\n")
+                for report in body["reports"]:
+                    obs = get_observation()
+                    used = len(obs["observation_urls"].keys())
+                    if LIMIT > used:
+                        add_observation(report["domain"])
+                    time.sleep(0.1)
+                body = "OK"
+                statusCode = 200
+            except Exception as e:
+                body = "request error"
+                body = str(e)
+                statusCode = 500
+        self.send_response(statusCode)
+        self.send_header("Content-type", "application/json")
+        self.send_header("content-length", len(body))
         self.end_headers()
         self.wfile.write(body.encode())
 
+
+with open("/config.yml") as f:
+    conf = yaml.safe_load(f)
+    set_keywords(conf)
 set_webhook_url()
 set_urlscan_api()
 
-ip = '0.0.0.0'
+ip = "0.0.0.0"
 port = 8080
 server = HTTPServer((ip, port), BaseHttpServer)
 server.serve_forever()
